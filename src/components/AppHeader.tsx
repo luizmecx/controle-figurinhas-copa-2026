414065d49afbbb0ea56004f85d18bf277a22a13b
luiz.felipe <felipecyrillo95@gmail.com>
2026-05-06 21:49:10 -0300

    LOGO FIFA

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -36,7 +36,7 @@ export function AppHeader({
           <div className="px-4 pt-4 pb-2 flex items-center gap-3">
             <div className="w-11 h-11 rounded-full bg-white flex items-center justify-center border-2 border-white shrink-0 shadow-sm overflow-hidden">
               <img 
-                src="https://seeklogo.com/images/F/fifa-world-cup-2026-logo-B3F0270DAA-seeklogo.com.png" 
+                src="https://s2-ge.glbimg.com/RGU5LEYpvpcPRro8r9wxAEvmltU=/0x0:2048x2048/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2023/q/Q/e3U1thRemgE9lEG0J4gQ/logo-fifa.jpg" 
                 alt="2026 Logo" 
                 className="w-full h-full object-cover p-1.5"
               />
